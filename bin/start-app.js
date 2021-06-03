7bb15dceca36f76a36b7a790afbcf71d63df9dea
Yoki Yu <yokiyuqian@gmail.com>
2021-06-03 11:56:50 +0800

    chore: cache config for build

--- bin/start-app.js
+++ bin/start-app.js
@@ -6,16 +6,27 @@ const path = require('path')
 
 const dirs = fs.readdirSync(path.join(__dirname, '../packages/templates'))
 
+const configFile = path.join(__dirname, './.cache.config.json')
+fs.ensureFileSync(configFile)
+const config = fs.readJsonSync(configFile, {
+  throws: false
+})
+
 inquirer
   .prompt({
     name: 'cra-template',
     type: 'list',
     message: 'Please select a cra-template to start',
-    default: dirs[0],
+    default: config?.lastTemplate || dirs[0],
     choices: dirs
   })
   .then(answer => {
     const scope = answer['cra-template']
+
+    fs.writeJsonSync(configFile, {
+      lastTemplate: scope
+    })
+
     spawn('lerna', ['run', 'start', '--scope', scope], {
       shell: true,
       stdio: 'inherit'
