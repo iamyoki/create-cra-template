707692843b7d48c6f73a5cdba79ea4e59d60ec7e
Yoki Yu <yokiyuqian@gmail.com>
2021-06-15 13:06:18 +0800

    feat: add second page

--- packages/templates/cra-template-popular/src/Routes.jsx
+++ packages/templates/cra-template-popular/src/Routes.jsx
@@ -5,12 +5,14 @@
 import { Router } from '@reach/router'
 import HomePage from './containers/HomePage'
 import PageWrapper from './containers/PageWrapper'
+import SecondPage from './containers/SecondPage'
 
 function Routes() {
   return (
     <Router id='router'>
       <PageWrapper path='/'>
         <HomePage path='home' label='Home' />
+        <SecondPage path='second' label='Second' />
       </PageWrapper>
     </Router>
   )
---
+++ packages/templates/cra-template-popular/src/containers/SecondPage/index.jsx
@@ -0,0 +1,13 @@
+/**
+ * SecondPage
+ */
+
+function SecondPage() {
+  return (
+    <div className='SecondPage'>
+      <h1>SecondPage</h1>
+    </div>
+  )
+}
+
+export default SecondPage
